819539fd7c3aeb137476383cf4ff0c74666e02b0
Ardanuc AKAR <ardanucakar@hotmail.com>
2019-05-14 17:33:29 +0300

    Proje Açıldı

---
+++ Veri Yapıları Dönem Projesi/Veri Yapıları Dönem Projesi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veri_Yapıları_Dönem_Projesi
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
